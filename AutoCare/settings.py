96efe3edbbc15ca0b04c84734f065217ab039dd2
vinit-sys <68093485+vinit-sys@users.noreply.github.com>
2021-12-26 10:44:01 +0530

    Update settings.py

--- AutoCare/settings.py
+++ AutoCare/settings.py
@@ -9,7 +9,8 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import mimetypes
+mimetypes.add_type("text/css", ".css", True)
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
