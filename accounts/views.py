b1ff8a33bce957344070685e2fa3b739d1e861a8
vinit-sys <68093485+vinit-sys@users.noreply.github.com>
2022-03-31 17:49:04 +0530

    Update views.py

--- accounts/views.py
+++ accounts/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render,HttpResponse,redirect
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate
 from django.contrib.auth import login as auth_login
-from django.contrib.auth.decorators import csrf_exempt
+from django.views.decorators.csrf import csrf_exempt
 # Create your views here.
 def index(request):
     return render(request,"login.html")
